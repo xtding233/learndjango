f6345535b65fa66dd282e5a1b155982eb9300f64
xtding233 <136866115+xtding233@users.noreply.github.com>
2024-08-31 13:01:43 -0400

    Implement dummy vote

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,9 @@ from django.template import loader
 from .models import Question, Choice
 # Create your views here.
 
+from django.db.models import F
+from django.urls import reverse
+
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     # template = loader.get_template("polls/index.html")
@@ -28,4 +31,16 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,
+                      'polls/detail.html',
+                      { "question" : question, "error_message": "Please select a choice."})
+    else:
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Always return an HttpReponseRedirect after successfully dealing with POST data. This prevents data from being
+        # posted twice if a user hits the Back button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
