e48245ffc5862ee9fe02d1c4237225d6301e2f7d
xtding233 <136866115+xtding233@users.noreply.github.com>
2024-09-01 11:43:14 -0400

    Add other tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 from django.db.models import F
@@ -14,7 +15,8 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         " Return the last five published questions (in alphabetical order)."
-        return Question.objects.order_by('-pub_date')[:5]
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
